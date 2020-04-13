a324be875c6e83bddbe314d3408d4bd75249c0c3
Paulo Barros <paulo.alfredo.barros@gmail.com>
2020-04-13 00:04:08 -0300

    feat(filters): Add parser to filter operators

---
+++ helpers/convert-query.js
@@ -0,0 +1,26 @@
+const { Op } = require('sequelize')
+
+/**
+ * Converte filtros da queryString para sequelize
+ * @param { Object } filters
+ * @returns { Object }
+ */
+module.exports = filters => {
+  const fields = Object.keys(filters)
+  return fields.reduce((acc, field) => {
+    const value = filters[field]
+    if (field.includes('__contains')) {
+      const fieldName = field.replace('__contains', '')
+      return Object.assign(acc, { [fieldName]: { [Op.like]: `%${value}%` } })
+    }
+    if (field.includes('__gte')) {
+      const fieldName = field.replace('__gte', '')
+      return Object.assign(acc, { [fieldName]: { [Op.gte]: value } })
+    }
+    if (field.includes('__lte')) {
+      const fieldName = field.replace('__lte', '')
+      return Object.assign(acc, { [fieldName]: { [Op.lte]: value } })
+    }
+    return Object.assign(acc, { [field]: value })
+  }, {})
+}
