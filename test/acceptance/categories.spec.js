c4ef54f99c3df38a9bd633500f596dcaba018c1d
Paulo Barros <paulo.alfredo.barros@gmail.com>
2020-04-19 02:54:22 -0300

    tests(categories): Add tests to sorting

--- test/acceptance/categories.spec.js
+++ test/acceptance/categories.spec.js
@@ -6,8 +6,8 @@ describe('[Acceptance] Categories', () => {
   let res
   describe('GET /categories', () => {
     const fixtures = [
-      { id: 1, name: 'Esportes' },
-      { id: 2, name: 'Decoração' },
+      { id: 1, name: 'Decoração' },
+      { id: 2, name: 'Esportes' },
       { id: 3, name: 'Informática' },
       { id: 4, name: 'Móveis' },
       { id: 5, name: 'Roupas' }
@@ -32,35 +32,31 @@ describe('[Acceptance] Categories', () => {
 
       it('should return list of categories', () => {
         const { _meta, data } = res.body
-        expect(_meta.count).to.be.eql(fixtures.length)
+        expect(_meta).to.have.property('count', fixtures.length)
         expect(data).to.be.eql(fixtures)
+      })
+
+      it('should default limit be 10', () => {
+        const { _meta } = res.body
+        expect(_meta).to.have.property('limit', 10)
+      })
+
+      it('should default offset be 0', () => {
+        const { _meta } = res.body
+        expect(_meta).to.have.property('offset', 0)
+      })
+
+      it('should default sort by name with order ASC', () => {
+        const { _meta, data } = res.body
+        expect(_meta).to.have.property('sort', 'name')
+        expect(_meta).to.have.property('order', 'ASC')
+        expect(data[0]).to.be.eql(fixtures[0])
+        expect(data[4]).to.be.eql(fixtures[4])
       })
     })
 
     context('with params', () => {
       context('pagination', () => {
-        context('without params', () => {
-          before(async () => {
-            res = await app.get('/categories').query({ limit: undefined, offset: undefined })
-          })
-
-          it('should return data', () => {
-            const { _meta, data } = res.body
-            expect(_meta.count).to.be.eql(fixtures.length)
-            expect(data).to.be.eql(fixtures)
-          })
-
-          it('should limit default be 10', () => {
-            const { _meta } = res.body
-            expect(_meta.limit).to.be.eql(10)
-          })
-
-          it('should offset default be 0', () => {
-            const { _meta } = res.body
-            expect(_meta.offset).to.be.eql(0)
-          })
-        })
-
         context('when use limit', () => {
           before(async () => {
             res = await app.get('/categories').query({ limit: 3 })
@@ -68,8 +64,8 @@ describe('[Acceptance] Categories', () => {
 
           it('should limit results', () => {
             const { _meta, data } = res.body
-            expect(_meta.count).to.be.eql(fixtures.length)
-            expect(_meta.limit).to.be.eql(3)
+            expect(_meta).to.have.property('count', fixtures.length)
+            expect(_meta).to.have.property('limit', 3)
             expect(data).to.be.eql([fixtures[0], fixtures[1], fixtures[2]])
           })
         })
@@ -81,43 +77,94 @@ describe('[Acceptance] Categories', () => {
 
           it('should start from offset', () => {
             const { _meta, data } = res.body
-            expect(_meta.count).to.be.eql(fixtures.length)
-            expect(_meta.offset).to.be.eql(3)
+            expect(_meta).to.have.property('count', fixtures.length)
+            expect(_meta).to.have.property('offset', 3)
             expect(data).to.be.eql([fixtures[3], fixtures[4]])
           })
         })
       })
-      context('sorting', () => {})
+      context('sorting', () => {
+        context('when use sort', () => {
+          before(async () => {
+            res = await app.get('/categories').query({ sort: 'id' })
+          })
+
+          it('should sort by gived value', () => {
+            const { _meta, data } = res.body
+            expect(_meta).to.have.property('sort', 'id')
+            expect(_meta).to.have.property('order', 'ASC')
+            expect(data[0]).to.be.eql(fixtures[0])
+            expect(data[4]).to.be.eql(fixtures[4])
+          })
+        })
+        context('when use order', () => {
+          before(async () => {
+            res = await app.get('/categories').query({ order: 'desc' })
+          })
+
+          it('should order by gived value', () => {
+            const { _meta, data } = res.body
+            expect(_meta).to.have.property('sort', 'name')
+            expect(_meta).to.have.property('order', 'DESC')
+            expect(data[0]).to.be.eql(fixtures[4])
+            expect(data[4]).to.be.eql(fixtures[0])
+          })
+        })
+
+        context('when sort is invalid', () => {
+          before(async () => {
+            res = await app.get('/categories').query({ sort: 'not_a_field' })
+          })
+          it('should return status 422 (Unprocessable Entity)', () => {
+            expect(res.status).to.be.eql(422)
+          })
+
+          it('should return error message', () => {
+            expect(res.body).to.be.eql({
+              error: 'ValidationError',
+              message: 'Field sort must be id|name'
+            })
+          })
+        })
+
+        context('when order is invalid', () => {
+          before(async () => {
+            res = await app.get('/categories').query({ order: 'foo-bar-def' })
+          })
+          it('should return status 422 (Unprocessable Entity)', () => {
+            expect(res.status).to.be.eql(422)
+          })
+
+          it('should return error message', () => {
+            expect(res.body).to.be.eql({
+              error: 'ValidationError',
+              message: 'Field order must be ASC|DESC'
+            })
+          })
+        })
+      })
       context('filters', () => {
-        context('when filter by "name"', () => {
+        context('when filter by name', () => {
           before(async () => {
             res = await app.get('/categories').query({ name: 'Informática' })
           })
 
-          it('should return status 200 (OK)', () => {
-            expect(res.status).to.be.eql(200)
-          })
-
-          it('should return only "Informática"', () => {
+          it('should return with exact name', () => {
             const { _meta, data } = res.body
-            expect(_meta.count).to.be.eql(1)
+            expect(_meta).to.have.property('count', 1)
             expect(data).to.be.eql([{ id: 3, name: 'Informática' }])
           })
         })
 
-        context('when filter by "name__contains"', () => {
+        context('when filter by name__contains', () => {
           before(async () => {
             res = await app.get('/categories').query({ name__contains: 'sport' })
           })
 
-          it('should return status 200 (OK)', () => {
-            expect(res.status).to.be.eql(200)
-          })
-
-          it('should return only "Esportes"', () => {
+          it('should return with partial name matching', () => {
             const { _meta, data } = res.body
-            expect(_meta.count).to.be.eql(1)
-            expect(data).to.be.eql([{ id: 1, name: 'Esportes' }])
+            expect(_meta).to.have.property('count', 1)
+            expect(data).to.be.eql([{ id: 2, name: 'Esportes' }])
           })
         })
       })
@@ -187,7 +234,7 @@ describe('[Acceptance] Categories', () => {
       })
     })
 
-    context('when "name" is missing', () => {
+    context('when name is missing', () => {
       before(async () => {
         res = await app.post('/categories').send({ name: '' })
       })
@@ -255,7 +302,7 @@ describe('[Acceptance] Categories', () => {
       })
     })
 
-    context('when "name" is missing', () => {
+    context('when name is missing', () => {
       before(async () => {
         res = await app.put(`/categories/${oldFixture.id}`).send({ name: '' })
       })
