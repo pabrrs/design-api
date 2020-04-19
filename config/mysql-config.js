a9274d9ea1f9a4ab1e02eaafd3e0a837155591b5
Paulo Barros <paulo.alfredo.barros@gmail.com>
2020-04-19 02:04:19 -0300

    feat(config): Add two dbs to test and run prod

--- config/mysql-config.js
+++ config/mysql-config.js
@@ -1,20 +1,22 @@
 const loader = require('@b2wads/env-o-loader')
 
-module.exports = Object.freeze(
+const mysqlConfig = Object.freeze(
   loader({
     defaults: {
       hostname: '127.0.0.1',
       username: 'root',
       password: '',
-      port: 3306,
-      database: 'design_api'
+      port: 3307,
+      database: 'design_api_test'
     },
     production: {
-      hostname: 'env:APP_MYSQL_HOSTNAME',
-      username: 'env:APP_MYSQL_USERNAME',
-      password: 'env:APP_MYSQL_PASSWORD',
-      port: 'env:APP_MYSQL_PORT',
-      database: 'env:APP_MYSQL_DATABASE'
+      hostname: '127.0.0.1',
+      username: 'root',
+      password: '',
+      port: 3306,
+      database: 'design_api'
     }
   })
 )
+
+module.exports = mysqlConfig
